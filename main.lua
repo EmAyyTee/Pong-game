6fe185ad30d545fdb032c3fec5c0b428d327906a
eMTee <hanzoswitchmail@gmail.com>
2023-08-02 16:52:24 +0200

    Fixed the code errors from the last commit

--- main.lua
+++ main.lua
@@ -29,11 +29,9 @@ function love.load()
         vsync = true
     })
 
-    player1Score = 0
-    player2Score = 0
 
-    player1Y = Paddle(10, 30, 5, 20)
-    player2Y = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    player1 = Paddle(10, 30, 5, 20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
 
     ball = Ball(VIRTUAL_WIDTH  / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
@@ -41,21 +39,28 @@ function love.load()
 end
 
 function love.update(dt)
+    -- player 1 movement
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0, player1Y + -PADDLE_SPEED * dt)
+        player1.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
 
+    -- player 2 movement
     if love.keyboard.isDown('up') then
-        player2Y = math.max(0,player2Y + -PADDLE_SPEED * dt)
+        player2.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('down') then
-        player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
     end
 
     if gameState == 'play' then
         ball:update(dt)
     end
+
     player1:update(dt)
     player2:update(dt)
 end
@@ -79,16 +84,17 @@ function love.draw()
     push:apply('start')
 
     love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.setFont(smallFont)
 
-    love.graphics.printf('Daj mi prace pls', 0, VIRTUAL_WIDTH / 2 - 6, VIRTUAL_HEIGHT, 'center')
+    if gameState == 'start' then
+        love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
 
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    player1:render()
+    player2:render()
+    ball:render()
 
-    love.graphics.rectangle('fill', 10, player1Y, 5, 20)
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
-    
     push:apply('end')
 end
